33566eef174f74115fee808f207ff49c2859d874
Michał Bartoszewski <michal.bartoszewski@yahoo.pl>
2012-07-22 18:07:09 +0200

    Now project is deployable ! And first bean implementation works !

--- ExpressDruk/ExpressDrukClient/src/main/java/pl/expressdruk/ejb/client/Main.java
+++ ExpressDruk/ExpressDrukClient/src/main/java/pl/expressdruk/ejb/client/Main.java
@@ -1,12 +1,23 @@
 package pl.expressdruk.ejb.client;
 
+import javax.ejb.EJB;
+import pl.expressdruk.ejb.remote.interfaces.ProductCrudOperationsBeanRemote;
+import pl.expressdruk.entities.Product;
+
 /**
  * Enterprise Application Client main class.
  *
  */
 public class Main {
     
+    @EJB
+    private static ProductCrudOperationsBeanRemote productCrudBean;
+    
     public static void main( String[] args ) {
-        System.out.println( "Hello World Enterprise Application Client!" );
+        System.out.println( "Hello World I am e1n's Expressdruk Enterprise Application Client!");
+        
+        Product prod = new Product("O yeah !", "To kurcze pieczaczki dziala ! Expressdruk bedzie potentatem !");
+        productCrudBean.persist(prod);
+        System.out.println("Ok po logowaniu !");
     }
 }
